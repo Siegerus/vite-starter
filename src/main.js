fccab974e5236395005a4c0045d55347503bfd62
Siegerus <ivanoffsiegerus@gmail.com>
2025-08-29 21:26:19 +0300

    removed all

--- src/main.js
+++ src/main.js
@@ -2,29 +2,4 @@
 import './scss/main.scss';
 
 //js
-// import { navMenu } from "./main-menu.js";
-// import { result } from './json';
-import './my-script.js';
-
-// const m = [{
-//         title: 'Home',
-//         pathLink: '/'
-//     },
-//     {
-//         title: 'About',
-//         pathLink: '/pages/about/'
-//     },
-//     {
-//         title: 'Node',
-//         pathLink: '/pages/node/'
-//     },
-//     {
-//         title: 'ООП',
-//         pathLink: '/pages/oop/'
-//     },
-// ];
-
-// navMenu(m, 'collapse', 'navbar-nav me-auto mb-2 mb-lg-0', 'nav-item', 'nav-link');
-
-// data.json
-// result();
+import './my-script.js'; 
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,13 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
-import { pageData } from './src/pages';
-
-const partDirs = [
-    'src/partials',
-    // 'src/pages/about',
-    // 'src/pages/node',
-    // 'src/pages/oop',
-];
 
 export default defineConfig({
     root: 'src',
     base: '',
     plugins: [handlebars({
-        context(pagePath) {
-            return pageData[pagePath];
-        },
-        // partialDirectory: resolve(__dirname, 'src/partials'),
-        partialDirectory: partDirs,
+ 
+        partialDirectory: resolve(__dirname, 'src/partials'),
         reloadOnPartialChange: true,
     })],
     build: {
@@ -27,10 +16,6 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'src/index.html'),
-                // about: resolve(__dirname, 'src/pages/about/index.html'),
-                // node: resolve(__dirname, 'src/pages/node/index.html'),
-                // oop: resolve(__dirname, 'src/pages/oop/index.html'),
-                // constructors: resolve(__dirname, 'src/pages/oop/constructors/index.html')
             }
         }
     },
