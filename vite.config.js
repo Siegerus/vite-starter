aff810c19959a50ed01c4a1a441b23c25f1fe8a3
Siegerus <ivanoffsiegerus@gmail.com>
2025-08-29 19:43:36 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import handlebars from 'vite-plugin-handlebars';
+import { pageData } from './src/pages';
+
+const partDirs = [
+    'src/partials',
+    'src/pages/about',
+    'src/pages/node',
+    'src/pages/oop',
+];
+
+export default defineConfig({
+    root: 'src',
+    base: '',
+    plugins: [handlebars({
+        context(pagePath) {
+            return pageData[pagePath];
+        },
+        // partialDirectory: resolve(__dirname, 'src/partials'),
+        partialDirectory: partDirs,
+        reloadOnPartialChange: true,
+    })],
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                about: resolve(__dirname, 'src/pages/about/index.html'),
+                node: resolve(__dirname, 'src/pages/node/index.html'),
+                oop: resolve(__dirname, 'src/pages/oop/index.html'),
+                constructors: resolve(__dirname, 'src/pages/oop/constructors/index.html')
+            }
+        }
+    },
+});
