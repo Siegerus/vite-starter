12d44d7d4d20f7bffced70b62bdb7738ece7f4a3
Siegerus <ivanoffsiegerus@gmail.com>
2025-08-29 23:03:07 +0300

    page+

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'src/index.html'),
-                main: resolve('src/*.html'),
+                main: resolve(__dirname, 'src/page.html'),
             }
         }
     },
